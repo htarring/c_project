f787176b3d15f4f680362fbaa566f2e73c176d84
htarring <htarring@outlook.com>
2018-04-27 15:51:39 +0800

    let me try

---
+++ LinuxSerial_Operation/Lserial_R_W.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* some reason cause my operation failed, but what that is???*/
+int main ()
+{
+    printf(" hello git!");
+    getchar();
+    getchar();
+
+}
+
+/**/
+int testuplaod()
+{
+    printf("Just a test!!!");
+}
